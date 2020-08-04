208213c1a6b3d5f8ce8104d4ab59be78fe420bec
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-04 19:38:03 -0400

    added components folder and built a usercards component with some placeholders

---
+++ react-github-user-card/src/Components/UserCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class UserCards extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>placeholder</h1>
+        );
+    }
+}
+ 
+export default UserCards;
