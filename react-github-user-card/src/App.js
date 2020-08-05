ea226b0dea4be0c87716428cab1ea583b2cf2f1f
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-04 21:08:21 -0400

    forgot axios import, setup state with get request using cdm

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
 import UserCards from './Components/UserCards';
+import axios from "axios"
 
 class App extends React.Component{
   constructor() {
     super()
     this.state = {
-      user: []
+      user: {}
     }
   }
 
   componentDidMount() {
     axios.get("https://api.github.com/users/richard-mcghee")
     .then((res) => {
-      console.log(res)
+      console.log(res.data)
+      this.setState({user: res.data})
+      console.log(this.state.user)
     })
     .catch((err) => {
       console.dir(err)
